301d73866eab0edf1fe59c2e83e355316b9266a6
Steven Bready <stevenbready@MacBook-Pro.local>
2017-11-14 13:38:26 -0700

    completed mini project

--- server/index.js
+++ server/index.js
@@ -1,15 +1,41 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mc = require( `./controllers/messages_controller` );
+const session = require('express-session')
+const createInitialSession = require('./middlewares/session');
+const filter = require('./middlewares/filter');
+require('dotenv').config();
 
 const app = express();
 
 app.use( bodyParser.json() );
-app.use( express.static( `${__dirname}/build` ) );
+app.use( express.static( `${__dirname}/../build` ) );
+
+app.use( session ({
+    resave: false,
+    saveUninitialized: true,
+    secret: process.env.SESSION_SECRET,
+    cookie: {
+        maxAge: (365 * 24 * 60 * 60 * 1000)
+    }
+}))
+
+
+app.use(createInitialSession)
+app.use( ( req, res, next ) => {
+    const { method } = req
+    if( req.method === 'POST' || req.method === 'PUT' ){
+        filter( req, res, next )
+    } else {
+        next()
+    }
+})
+
 
 const messagesBaseUrl = "/api/messages";
 app.post( messagesBaseUrl, mc.create );
 app.get( messagesBaseUrl, mc.read );
+app.get( `${messagesBaseUrl}/history`, mc.history)
 app.put( `${messagesBaseUrl}`, mc.update );
 app.delete( `${messagesBaseUrl}`, mc.delete );
 
